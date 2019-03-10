e7a2054e80361df47fc2fe7e2cb18c5500f70bb6
rattle99 <24495512+rattle99@users.noreply.github.com>
2019-03-10 15:00:14 +0530

    Add more flags

--- youpod.py
+++ youpod.py
@@ -10,12 +10,20 @@ def main():
     	in your terminal!')
     parser.add_argument('-v', '--version', action = 'version', version = '%(prog)s v0.1.0',  help = 'display version')
     parser.add_argument('-d', dest = 'url', action = 'store', help = 'download audio directly from given URL')
-
+    parser.add_argument('-l', '--list', dest = 'list_podcasts' ,action = 'store_true', help = 'list all downloaded podcasts')
+    parser.add_argument('-rm', dest = 'name', action = 'store', help = 'delete podcast (use "all" to delete all podcasts)')
+    
     args = parser.parse_args()
     url = args.url
-
+    rm = args.name
+    ls = args.list_podcasts
+    
     if url:
         print(url)
+    if rm:
+        print(rm)
+    if ls:
+        print(ls)
 
 if __name__ == '__main__':
     main()
